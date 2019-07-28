5245e8f2c30b7c3c6aed120c6280d97ab6009e7a
RingoWinbee <13922924658@163.com>
2019-07-29 01:23:24 +0800

    备份zuul

---
+++ zuulDemo/src/main/java/com/example/demo/MyZuulFallBack.java
@@ -0,0 +1,65 @@
+package com.example.demo;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+
+/**
+ * @author hzk
+ * @date 2018/6/28
+ */
+@Component
+public class MyZuulFallBack implements FallbackProvider {// ZuulFallbackProvider过时,推荐使用FallbackProvider
+
+	@Override
+	public String getRoute() {
+		// TODO Auto-generated method stub
+		return "*";
+	}
+
+	@Override
+	public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+		// TODO Auto-generated method stub
+		return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.BAD_REQUEST;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return HttpStatus.BAD_REQUEST.value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return HttpStatus.BAD_REQUEST.getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream((" happened error!"+getRoute()).getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders httpHeaders = new HttpHeaders();
+                httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+                return httpHeaders;
+            }
+        };
+	}
+}
